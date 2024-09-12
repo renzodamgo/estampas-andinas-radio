17e2963a9e11bea257a0a56dfcb74cc5bd4d5b22
Renzo Damian <renzo.damian.go@gmail.com>
2024-09-11 19:20:11 -0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <div className="rounded-t-2xl  px-4 pt-4 border border-gray-500 backdrop-blur-2xl backdrop-brightness-80">
         <h1 className="text-2xl font-bold pb-4">Estampas Andinas</h1>
         <iframe
-          src="https://s62.radiolize.com/public/estampas_andinas_txe8wq/embed?theme=light"
+          src="https://s62.radiolize.com/public/estampas_andinas_4jpneu/embed?theme=light"
           className="w-full min-h-[140px]"
         ></iframe>
       </div>
